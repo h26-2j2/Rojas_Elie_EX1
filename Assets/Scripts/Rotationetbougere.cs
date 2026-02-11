6a971ecfca6d6c22fc2cb4907cc779aa3bd5d79f
SchoolElieRojas <e6375313@cmaisonneuve.qc.ca>
2026-02-11 14:49:36 -0500

    Initial commit

---
+++ Assets/Scripts/Rotationetbougere.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Rotationetbougere : MonoBehaviour
+{
+    private int bloxX;
+    private int bloxY;
+    public float positionMax = 12f;
+    public float positionMin = -12f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Rojas_Elie_EX1
+Exercice 2j2
